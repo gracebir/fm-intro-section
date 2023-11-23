579917509d4a1f375c006540b8a7d4301c1f5979
gracebir <grace.devolop@gmail.com>
2023-11-23 19:04:51 +0200

    get started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const App = () => {
+    return (
+        <div className='font-poppins h-screen'>
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+const Hero = () => {
+    return <div>hero</div>;
+};
+
+export default Hero;
